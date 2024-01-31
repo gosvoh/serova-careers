09ac45ce86eb5e99ddbd28af9e4e9cfb83e5fa01
Aleksey Vokhmin <vohmina2011@yandex.ru>
2024-01-31 13:02:12 +0300

    Added more logs

--- src/app/ContactSubmitAction.ts
+++ src/app/ContactSubmitAction.ts
@@ -3,18 +3,22 @@
 import { revalidatePath } from "next/cache";
 import nodemailer from "nodemailer";
 
+const user = process.env.MAIL_USER;
+const pass = process.env.MAIL_PASS;
+const privateKey = process.env.DKIM_PRIVATE_KEY;
+const to = process.env.RECEIVE_EMAIL;
+
+console.log(user, pass, privateKey, to);
+
 const transporter = nodemailer.createTransport({
   host: "smtp.timeweb.ru",
-  auth: {
-    user: process.env.MAIL_USER!,
-    pass: process.env.MAIL_PASS!,
-  },
+  auth: { user, pass },
   port: 465,
   secure: true,
   dkim: {
     domainName: "serova.careers",
     keySelector: "mail",
-    privateKey: process.env.DKIM_PRIVATE_KEY!,
+    privateKey: privateKey!,
   },
 });
 
@@ -24,13 +28,15 @@ const exit = (status: number) => {
 };
 
 export default async function contactSubmitAction(_: any, formData: FormData) {
+  if (!user || !pass || !privateKey || !to) exit(500);
+
   const name = formData.get("name");
   const login = formData.get("login");
 
   if (!name || !login) return exit(400);
 
   await transporter.sendMail({
-    to: process.env.RECEIVE_EMAIL,
+    to,
     subject: "[SEROVA.CAREERS] Обратная связь",
     text: `[${name}] ${login}`,
     html: `<html><body>[${name}] ${login}</body></html>`,
