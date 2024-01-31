0dd1d2b1f633f6c0e797277a786ae3ce47c09338
Aleksey Vokhmin <vohmina2011@yandex.ru>
2024-01-31 14:04:33 +0300

    Attempt to fix mail service

--- src/app/ContactSubmitAction.ts
+++ src/app/ContactSubmitAction.ts
@@ -3,31 +3,17 @@
 import { revalidatePath } from "next/cache";
 import nodemailer from "nodemailer";
 
-const user = process.env.MAIL_USER;
-const pass = process.env.MAIL_PASS;
-const privateKey = process.env.DKIM_PRIVATE_KEY;
-const to = process.env.RECEIVE_EMAIL;
-
-console.log(user, pass, privateKey, to);
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.timeweb.ru",
-  auth: { user, pass },
-  port: 465,
-  secure: true,
-  dkim: {
-    domainName: "serova.careers",
-    keySelector: "mail",
-    privateKey: privateKey!,
-  },
-});
-
 const exit = (status: number) => {
   revalidatePath("/");
   return { status };
 };
 
 export default async function contactSubmitAction(_: any, formData: FormData) {
+  const user = process.env.MAIL_USER;
+  const pass = process.env.MAIL_PASS;
+  const privateKey = process.env.DKIM_PRIVATE_KEY;
+  const to = process.env.RECEIVE_EMAIL;
+
   if (!user || !pass || !privateKey || !to) exit(500);
 
   const name = formData.get("name");
@@ -35,12 +21,24 @@ export default async function contactSubmitAction(_: any, formData: FormData) {
 
   if (!name || !login) return exit(400);
 
-  await transporter.sendMail({
-    to,
-    subject: "[SEROVA.CAREERS] Обратная связь",
-    text: `[${name}] ${login}`,
-    html: `<html><body>[${name}] ${login}</body></html>`,
-  });
+  await nodemailer
+    .createTransport({
+      host: "smtp.timeweb.ru",
+      auth: { user, pass },
+      port: 465,
+      secure: true,
+      dkim: {
+        domainName: "serova.careers",
+        keySelector: "mail",
+        privateKey: privateKey!,
+      },
+    })
+    .sendMail({
+      to,
+      subject: "[SEROVA.CAREERS] Обратная связь",
+      text: `[${name}] ${login}`,
+      html: `<html><body>[${name}] ${login}</body></html>`,
+    });
 
   return exit(200);
 }
