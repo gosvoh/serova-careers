4a5993ac9f18a0656c5b5d09d4894b5bdb2bc796
Aleksey Vokhmin <vohmina2011@yandex.ru>
2024-02-11 15:07:26 +0300

    Changed page type to dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ import Link from "next/link";
 import fs from "node:fs";
 import ContactForm from "./ContactForm";
 
+export const dynamic = "force-dynamic";
 export const revalidate = 600;
 
 let materials: { [title: string]: string } = {};
